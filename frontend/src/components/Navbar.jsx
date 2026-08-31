167464dd67164cfba251b179a800996c23c8cdf2
Sahil Hinge <sahilhinge89@gmail.com>
2026-08-31 18:44:37 +0530

    add navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                File<span>Vault</span>
+            </div>
+
+            <div className="nav-right">
+                <span>My Files</span>
+                <div className="avatar">S</div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
