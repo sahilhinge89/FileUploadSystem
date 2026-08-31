f896eda6c1aa7347d4390c485075868bec02f6a1
Sahil Hinge <sahilhinge89@gmail.com>
2026-08-31 18:41:36 +0530

    setup backend server and middleware

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,51 @@
-//Create App
- const express = require("express");
- const app = express();
+// Create App
+const express = require("express");
+const cors = require("cors");
 
-// Find out port 
+const app = express();
+
+
+// Find out port
 require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
+
 
 // Add middlewares
 app.use(express.json());
-const fileupload = require("express-fileupload");
-app.use(fileupload({
-    useTempFiles: true,
-     tempFileDir: "/tmp/"
-}
- ));
 
-// Connect database 
+// Allow frontend to connect with backend
+app.use(cors());
+
+const fileupload = require("express-fileupload");
+
+app.use(
+    fileupload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/"
+    })
+);
+
+
+// Connect database
 const connectDB = require("./config/database");
+
 connectDB.connect();
 
-// Connect cloudinary
 
-const cloudinary = require('./config/cloudinary');
+// Connect Cloudinary
+const cloudinary = require("./config/cloudinary");
+
 cloudinary.cloudinaryConnect();
 
-// Mount api route
+
+// Mount API route
 const Upload = require("./routes/fileRoutes");
-app.use('/api/v1/upload',Upload);
+
+app.use("/api/v1/upload", Upload);
+
 
 // Activate server
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`App is running successfully ${PORT}`);
-})
+});
